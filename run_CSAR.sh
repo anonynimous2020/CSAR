b74b6150d5296ff35de2a9211ec82b5a356fb4b3
anonynimous2020 <1876331732@qq.com>
2020-09-06 23:07:29 +0800

    1

--- run_CSAR.sh
+++ run_CSAR.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 GPU_ID=$1
-
-for SEED in 1 2 3 ; do
- CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --approach CSAR --alpha 0.1 --beta 0.1 --gamma 0.03 --logname $SEED'_1' --seed $SEED
-done
+SEED=$2
+CUDA_VISIBLE_DEVICES=$GPU_ID python main.py --approach CSAR --alpha 0.1 --beta 0.1 --gamma 0.03 --logname $SEED'_CSAR' --seed $SEED
